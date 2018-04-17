b0ce1966c0c1bb9473021db098a2d461e09bf5f7
Matt Ford <mateoford@gmail.com>
2018-04-17 17:16:45 -0500

    P_C: ABAQUS files and plots for lateral buckling Riks

---
+++ data/doe/doe_lat_buckling_riks/_run_0.bat
@@ -0,0 +1,3 @@
+call abaqus interactive job=Tn0.00_riks input=Tn0.00_riks.inp
+call abaqus interactive job=Tn0.40_riks input=Tn0.40_riks.inp
+call abaqus interactive job=Tn0.80_riks input=Tn0.80_riks.inp
---
+++ data/doe/doe_lat_buckling_riks/_run_3.bat
@@ -0,0 +1,2 @@
+call abaqus interactive job=Tn0.30_riks input=Tn0.30_riks.inp
+call abaqus interactive job=Tn0.70_riks input=Tn0.70_riks.inp
