6076ffebc4e50548175cf5139521a06bdddb511a
Matt Ford <mateoford@gmail.com>
2018-09-14 14:59:25 -0500

    #66: Re-run rad buckling sims with wheel library parameters

---
+++ data/doe/rad_buckling_beam/_run_3.bat
@@ -0,0 +1,38 @@
+call abaqus interactive job=cheap_MTB_0.30_preT input=cheap_MTB_0.30_preT.inp
+call abaqus interactive job=cheap_MTB_0.30_collapse input=cheap_MTB_0.30_collapse.inp oldjob=cheap_MTB_0.30_preT
+call abaqus interactive job=cheap_MTB_0.70_preT input=cheap_MTB_0.70_preT.inp
+call abaqus interactive job=cheap_MTB_0.70_collapse input=cheap_MTB_0.70_collapse.inp oldjob=cheap_MTB_0.70_preT
+call abaqus interactive job=vintage_road_0.00_preT input=vintage_road_0.00_preT.inp
+call abaqus interactive job=vintage_road_0.00_collapse input=vintage_road_0.00_collapse.inp oldjob=vintage_road_0.00_preT
+call abaqus interactive job=vintage_road_0.40_preT input=vintage_road_0.40_preT.inp
+call abaqus interactive job=vintage_road_0.40_collapse input=vintage_road_0.40_collapse.inp oldjob=vintage_road_0.40_preT
+call abaqus interactive job=vintage_road_0.80_preT input=vintage_road_0.80_preT.inp
+call abaqus interactive job=vintage_road_0.80_collapse input=vintage_road_0.80_collapse.inp oldjob=vintage_road_0.80_preT
+call abaqus interactive job=racing_700_0.10_preT input=racing_700_0.10_preT.inp
+call abaqus interactive job=racing_700_0.10_collapse input=racing_700_0.10_collapse.inp oldjob=racing_700_0.10_preT
+call abaqus interactive job=racing_700_0.50_preT input=racing_700_0.50_preT.inp
+call abaqus interactive job=racing_700_0.50_collapse input=racing_700_0.50_collapse.inp oldjob=racing_700_0.50_preT
+call abaqus interactive job=racing_700_0.90_preT input=racing_700_0.90_preT.inp
+call abaqus interactive job=racing_700_0.90_collapse input=racing_700_0.90_collapse.inp oldjob=racing_700_0.90_preT
+call abaqus interactive job=small_0.20_preT input=small_0.20_preT.inp
+call abaqus interactive job=small_0.20_collapse input=small_0.20_collapse.inp oldjob=small_0.20_preT
+call abaqus interactive job=small_0.60_preT input=small_0.60_preT.inp
+call abaqus interactive job=small_0.60_collapse input=small_0.60_collapse.inp oldjob=small_0.60_preT
+call abaqus interactive job=small_0.95_preT input=small_0.95_preT.inp
+call abaqus interactive job=small_0.95_collapse input=small_0.95_collapse.inp oldjob=small_0.95_preT
+call abaqus interactive job=track_0.30_preT input=track_0.30_preT.inp
+call abaqus interactive job=track_0.30_collapse input=track_0.30_collapse.inp oldjob=track_0.30_preT
+call abaqus interactive job=track_0.70_preT input=track_0.70_preT.inp
+call abaqus interactive job=track_0.70_collapse input=track_0.70_collapse.inp oldjob=track_0.70_preT
+call abaqus interactive job=tandem_0.00_preT input=tandem_0.00_preT.inp
+call abaqus interactive job=tandem_0.00_collapse input=tandem_0.00_collapse.inp oldjob=tandem_0.00_preT
+call abaqus interactive job=tandem_0.40_preT input=tandem_0.40_preT.inp
+call abaqus interactive job=tandem_0.40_collapse input=tandem_0.40_collapse.inp oldjob=tandem_0.40_preT
+call abaqus interactive job=tandem_0.80_preT input=tandem_0.80_preT.inp
+call abaqus interactive job=tandem_0.80_collapse input=tandem_0.80_collapse.inp oldjob=tandem_0.80_preT
+call abaqus interactive job=high_wheel_0.10_preT input=high_wheel_0.10_preT.inp
+call abaqus interactive job=high_wheel_0.10_collapse input=high_wheel_0.10_collapse.inp oldjob=high_wheel_0.10_preT
+call abaqus interactive job=high_wheel_0.50_preT input=high_wheel_0.50_preT.inp
+call abaqus interactive job=high_wheel_0.50_collapse input=high_wheel_0.50_collapse.inp oldjob=high_wheel_0.50_preT
+call abaqus interactive job=high_wheel_0.90_preT input=high_wheel_0.90_preT.inp
+call abaqus interactive job=high_wheel_0.90_collapse input=high_wheel_0.90_collapse.inp oldjob=high_wheel_0.90_preT
